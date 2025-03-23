1e80ee7580d9e84e99573c40c56c90282359c0cd
VeCtORbytes <sarthakgupta@sarthaks-MacBook-Air-2.local>
2025-03-23 22:13:23 +0530

    image added

--- frontend/src/Landing_page/home/Hero.js
+++ frontend/src/Landing_page/home/Hero.js
@@ -10,7 +10,7 @@ function Hero() {
       <div className='container p-5'>
         <div className='row text-center'>
        
-         <img src='media/images/homeHero.png' alt='Hero image' className='mb-5' />
+         <img src='Media/images/homeHero.png' alt='Hero' className='mb-5' />
          <h1 className='mt-5'>Invest in Everything</h1>
          <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
          <Link to={"/signup"} className='p-2 btn btn-primary' style={{width:"20%", margin :"0 auto"}} >SignUp Now</Link>
